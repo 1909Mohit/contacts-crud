488917904fecdefc03f3ff351d10d668d05b05bc
1909Mohit <mohit1909.mk@gmail.com>
2022-06-16 18:18:39 +0530

    fixing more issues

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const cors = require("cors");
+
 const router = jsonServer.router("./db.json");
 const middlewares = jsonServer.defaults({
     static: "./build",
@@ -15,7 +15,7 @@ server.use(
 );
 
 server.use(router);
-server.use(cors());
+
 server.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });     
